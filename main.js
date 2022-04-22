8c34b40fb7dac1a46a77922015320ab87ca2ca8f
Rafael <rafesilvadesouza@gmail.com>
2022-04-22 17:49:35 -0300

    I'd say version 1.0

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
+//  =============== Variables ================= //
+
 let display = document.querySelector('input')
+
+let dragValue
+
+const calculator = document.querySelector('.calculator')
 
 const button = document.querySelectorAll('.grid-item')
 const list = document.querySelector('ul')
 
+const history = document.querySelector('.history')
+const listItems = document.querySelectorAll('.history li')
+
 const deleteButton = document.querySelector('.delete')
 const clearButton = document.querySelector('.clear')
 const closeButton = document.querySelector('.close')
+const removeButton = document.querySelector('.history .removeButton')
+const editButton = document.querySelector('.editButton')
+
 const modal = document.querySelector('.modal-wraper')
 const instructions = document.getElementById('instructions')
-const nav = document.querySelector('nav')
+const nav = document.getElementById('nav')
 const toggleNav = document.querySelector('.toggle-nav')
+
+// =========== Functions =============== //
 
 button.forEach(button => {
   button.addEventListener('click', () => {
@@ -26,6 +40,11 @@ const Delete = {
 
   clear() {
     display.value = ''
+  },
+
+  async remove(li) {
+    await li.parentNode.remove()
+    window.location.reload()
   }
 }
 
@@ -34,9 +53,11 @@ const Calculation = {
     let dispalyString = display.value
     let item = document.createElement('li')
     item.innerHTML = `
-    <div class='result'>${dispalyString} = ${eval(dispalyString)}</div>
-     <div class='editButton'>E</div>
-     <div class='removeButton'>X</div>`
+    <div class='name' contentEditable="true">-----</div>
+    <div class='result' contentEditable="true">${dispalyString} = ${eval(
+      dispalyString
+    )}</div>
+     <div class='removeButton' onclick="Delete.remove(this)">X</div>`
     list.appendChild(item)
     display.value = eval(dispalyString)
   }
@@ -51,10 +72,13 @@ const Modal = {
 const Nav = {
   toggle() {
     nav.classList.toggle('deactive')
+    toggleNav.classList.toggle('closed')
   }
 }
 
-window.onclick = () => {
+// ================== General click functions ================ //
+
+calculator.onclick = () => {
   display.focus()
 }
 
@@ -75,8 +99,6 @@ toggleNav.addEventListener('click', () => {
 })
 
 document.addEventListener('keydown', e => {
-  console.log(e.key)
-
   if (e.key == 'Escape') {
     Modal.toggle()
   }
@@ -88,8 +110,32 @@ display.addEventListener('keyup', e => {
   }
 })
 
-document.addEventListener('keydown', e => {
+display.addEventListener('keydown', e => {
   if (e.key == 'Enter') {
     Calculation.calc()
   }
 })
+
+// ==================== Drag ===================== //
+
+function move(element) {
+  history.onmousedown = function () {
+    dragValue = history
+  }
+}
+
+document.onmouseup = function (e) {
+  dragValue = null
+}
+
+document.onmousemove = function (e) {
+  if (dragValue != null) {
+    history.style.position = 'absolute'
+
+    var x = e.pageX
+    var y = e.pageY
+
+    dragValue.style.left = x + 'px'
+    dragValue.style.top = y + 'px'
+  }
+}
